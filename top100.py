f476e321e3f50a5766b63cc4418111d030615b04
Alexandre Ventura <alexandreventura630@gmail.com>
2024-11-08 18:05:07 -0300

    Update graphs

--- top100.py
+++ top100.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+import plotly.graph_objects as go
 
 st.set_page_config(layout="wide")
 
@@ -14,8 +15,34 @@ max_price = st.sidebar.slider("Intervalo de Preço", price_min, price_max, price
 df_books = df_top100_books[df_top100_books["book price"] <= max_price]
 df_books
 
-fig = px.bar(df_books["year of publication"].value_counts())
-fig2 = px.histogram(df_books["rating"])
+fig = go.Figure()
+fig.add_trace(go.Histogram(
+    x=df_books["year of publication"],
+    xbins=dict(
+        size=5
+    ),
+    marker_color='red',
+    opacity=0.70
+))
+
+fig.update_layout(
+    title_text = "Ano das Publicações",
+    xaxis_title_text='Anos',
+    yaxis_title_text='Quantidade'
+)
+
+fig2 = go.Figure()
+fig2.add_trace(go.Histogram(
+    x=df_books["rating"],
+    marker_color='red',
+    opacity=0.70
+))
+
+fig2.update_layout(
+    title_text = "Classificação por quantidade",
+    xaxis_title_text='Avaliação',
+    yaxis_title_text='Quantidade'
+)
 
 col1, col2 = st.columns(2)
 col1.plotly_chart(fig)
